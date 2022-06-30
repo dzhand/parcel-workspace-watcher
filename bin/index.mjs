526dd9970643af42b4db886a9f2d88d5a5e874e7
rbp <r@b.p>
2022-06-30 20:26:16 +0800

    fix

--- bin/index.mjs
+++ bin/index.mjs
@@ -4,13 +4,21 @@ import chokidar from 'chokidar'
 import fs from 'fs'
 import path from 'path'
 import url from 'url'
-
 const packagePath=path.join(process.cwd(),'./package.json');
 const packageObj=JSON.parse(fs.readFileSync(packagePath,'utf-8'))
 
-const watcher=chokidar.watch(packageObj.workspaces||packageObj.workspaces.packages||[],{})
+console.log(packageObj.workspaces)
 
-console.log('watcher started')
+let watchList=[]
+if(packageObj.workspaces){
+    if(packageObj.workspaces.length){
+        watchList=[...packageObj.workspaces]
+    }else{
+        watchList=[...packageObj.workspaces.packages]
+    }
+}
+const watcher=chokidar.watch(watchList,{})
+console.log('watcher started',JSON.stringify(watchList))
 
 watcher.on('change',path=>{
     console.log(path,'change')
